b5da654e07816d392232c18d7d153e06064d0571
shiruizhi <shiruizhi@blued.com>
2022-02-08 18:45:15 +0800

    feat: add cc

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,9 +25,23 @@ const Layout = ({ location, title, children }) => {
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+        <span>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.com">Gatsby</a>
+        </span>
+        <span>
+          <a
+            rel="license"
+            href="http://creativecommons.org/licenses/by-nc-sa/4.0/"
+          >
+            <img
+              alt="知识共享许可协议"
+              style={{borderWidth: 0}}
+              src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png"
+            />
+          </a>
+        </span>
       </footer>
     </div>
   )
