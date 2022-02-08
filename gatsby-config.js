4d1a6cda8003cf9fe62efcfa3d46e67036c71e81
shiruizhi <shiruizhi@blued.com>
2022-02-08 14:19:35 +0800

    init project

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,12 +51,12 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: `ADD YOUR TRACKING ID HERE`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: `G-8VH2YX0LY0`,
+      },
+    },
     {
       resolve: `gatsby-plugin-feed`,
       options: {
