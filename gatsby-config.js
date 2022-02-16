688afcc14e40e84c518be055fd5d57815e340948
shiruizhi <shiruizhi@blued.com>
2022-02-16 13:32:31 +0800

    update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `ccLin Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `cclin`,
+      summary: `an elementary school student`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+    description: `A starter blog`,
+    siteUrl: `https://cclin.org`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `cclinet1`,
     },
   },
   plugins: [
